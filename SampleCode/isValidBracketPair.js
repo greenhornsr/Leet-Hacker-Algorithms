f7889cc685b0549415136986857b9232eaa7000b
greenhornsr <srollins11279@gmail.com>
2020-05-18 14:04:13 -0700

    added check for pipes

--- SampleCode/isValidBracketPair.js
+++ SampleCode/isValidBracketPair.js
@@ -1,5 +1,6 @@
 // Source: https://repl.it/student/submissions/12648442
 
+// Does NOT CHECK FOR PIPES
 var isValid = function(str) {
     let check_arr = [];
     for(const el of str){
@@ -23,9 +24,46 @@ var isValid = function(str) {
     return check_arr.length ? false : true;
 };
 
+// Includes check for '|'
+var isValidPlusPipes = function(str) {
+    let check_arr = [];
+    for(const el of str){
+        let last = check_arr[check_arr.length-1]
+        if(el === '(' || el === '[' || el === '{'){
+            check_arr.push(el);
+        }
+        if(el === '|' && last !== '|'){
+            check_arr.push(el)
+        }
+        else if(check_arr.length){
+            
+            if(last === '(' && el === ')'){
+                check_arr.pop();
+            }
+            else if(last === '[' && el === ']'){
+                check_arr.pop();
+            }    
+            else if(last === '{' && el === '}'){
+                check_arr.pop();
+            }
+            else if(last === '|' && el === '|'){
+                check_arr.pop()
+            }
+        }
+    }
+    console.log(check_arr.length ? false : true)
+    return check_arr.length ? false : true;
+};
+
 // COMPLETED, PASSES!
-// TESTS
+// TESTS without Pipes ||
 const valstr = '{[()]}'
 const invalstr = '{[(()]}'
 isValid(valstr)
 isValid(invalstr)
+
+// TESTS with Pipes ||
+const valstrpluspipes = '{[(||)]}'
+const invalstrpluspipes = '{[(|(|)|)]}'
+isValidPlusPipes(valstr)
+isValidPlusPipes(invalstr)
