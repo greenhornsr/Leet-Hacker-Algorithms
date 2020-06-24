d72b4108cf703bb03f71192f1cd6ffe45401591b
greenhornsr <srollins11279@gmail.com>
2020-06-23 19:55:35 -0700

    added findlargestnuminrotatedlist.js.  Completed, passes.  Should be rated easy imo.

---
+++ BinarySearchIO/JavaScript/Completed/findlargestnuminrotatedlist.js
@@ -0,0 +1,29 @@
+// SOURCE: https://binarysearch.io/room/Empty-string-20807
+// DIFFICULTY: MEDIUM
+
+/*
+Find the largest number in a rotated sorted list
+You are given a list of integers nums that is sorted in ascending order and is rotated at some pivot point. Find the maximum number in the rotated list.
+
+Constraints:
+    Length of nums is at most 5000
+*/
+
+function solve(arr) {
+    // Write your code here
+    // const result = Math.max(...arr)
+    return Math.max(...arr)
+}
+
+// COMPLETED, PASSES!
+    // Finished in 3 minutes
+
+/*
+Success!
+Your submission took 2 milliseconds.
+Your submission was faster than 100.00% of other javascript submissions for this question.
+*/
+
+// TESTS
+let myinput = [6, 7, 8, 1, 4]
+solve(myinput)
