b84346c0be6d1c9215a675e3d9c0ffe5018b5852
greenhornsr <srollins11279@gmail.com>
2020-06-26 12:10:19 -0700

    added selectallsql.sql.  completed and passes

---
+++ HackerRank/SQL/Completed/selectallsql.sql
@@ -0,0 +1,20 @@
+-- SOURCE: https://www.hackerrank.com/challenges/select-all-sql/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+-- DIFFICULTY: EASY
+
+/*
+Query all columns (attributes) for every row in the CITY table.
+
+Input Format
+The CITY table is described as follows:
+    FIELD       |       TYPE
+    ID          |       NUMBER
+    NAME        |       VARCHAR2(17)
+    COUNTRYCODE |       VARCHAR2(3)
+    DISTRICT    |       VARCHAR2(20)
+    POPULATION  |       NUMBER  
+*/
+
+-- QUERY
+SELECT * FROM CITY;
+
+-- COMPLETED, PASSES!
