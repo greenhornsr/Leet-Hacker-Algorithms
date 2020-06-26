64025c8f9dbbb80215f2560618eef98b0cd06f39
greenhornsr <srollins11279@gmail.com>
2020-06-26 12:00:46 -0700

    added revisingtheselectquery2.sql

---
+++ HackerRank/SQL/revisingtheselectquery2.sql
@@ -0,0 +1,18 @@
+-- SOURCE:   https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+-- DIFFICTULTY: EASY
+
+/*
+Query the names of all American cities in CITY with populations larger than 120000. The CountryCode for America is USA.
+
+Input Format
+
+The CITY table is described as follows:
+    FIELD       |       TYPE
+    ID          |       NUMBER
+    NAME        |       VARCHAR2(17)
+    COUNTRYCODE |       VARCHAR2(3)
+    DISTRICT    |       VARCHAR2(20)
+    POPULATION  |       NUMBER  
+*/
+
+-- QUERY
