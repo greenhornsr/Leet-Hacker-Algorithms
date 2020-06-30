bf3bb6f13cd8a360bcd1e8fab6e7782175394d13
greenhornsr <srollins11279@gmail.com>
2020-06-29 20:30:32 -0700

    added wostation6.sql.  completed, passed

---
+++ HackerRank/SQL/wostation6.sql
@@ -0,0 +1,26 @@
+-- SOURCE: https://www.hackerrank.com/challenges/weather-observation-station-6/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+-- DIFFICULTY: EASY
+
+/*
+Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+
+Input Format
+The STATION table is described as follows:
+    FIELD       |       TYPE
+    ------------------------------------
+    ID          |       NUMBER
+    CITY        |       VARCHAR2(21)
+    STATE       |       VARCHAR2(2)
+    LAT_N       |       NUMBER
+    LONG_W      |       NUMBER  
+*/
+
+-- QUERY
+SELECT CITY FROM STATION
+WHERE LOCATE("a", CITY) = 1
+OR LOCATE("e", CITY) = 1
+OR LOCATE("i", CITY) = 1
+OR LOCATE("o", CITY) = 1
+OR LOCATE("u", CITY) = 1
+
+-- COMPLETED, PASSED!
