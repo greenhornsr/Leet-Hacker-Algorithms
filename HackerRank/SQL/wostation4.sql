1eb476048d79df4cad1f18c92b99a58707fb9052
greenhornsr <srollins11279@gmail.com>
2020-06-30 23:15:28 -0700

    adjusted wostation4.sql

--- HackerRank/SQL/wostation4.sql
+++ HackerRank/SQL/wostation4.sql
@@ -5,7 +5,6 @@
 Let N be the number of CITY entries in STATION, and let N be the number of distinct CITY names in STATION; query the value of N - N from STATION. In other words, find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
 
 Input Format
-
 The STATION table is described as follows:
     FIELD       |       TYPE
     ------------------------------------
