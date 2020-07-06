c012486f3e9ae2fac7ef01fe3c551bb2f5fa0aa8
greenhornsr <srollins11279@gmail.com>
2020-07-06 16:24:03 -0700

    added wostation.sql

---
+++ HackerRank/SQL/wostation7.sql
@@ -0,0 +1,22 @@
+-- SOURCE: https://www.hackerrank.com/challenges/weather-observation-station-7/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+-- DIFFICULTY: EASY
+
+/*
+Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+
+Input Format
+The STATION table is described as follows:
+    FIELD       |       TYPE
+    ------------------------------------
+    ID          |       NUMBER
+    CITY        |       VARCHAR2(21)
+    STATE       |       VARCHAR2(2)
+    LAT_N       |       NUMBER
+    LONG_W      |       NUMBER  
+*/
+
+-- QUERY
+
+
+
+-- COMPLETED, PASSED!
